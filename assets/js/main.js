88561660db06f4b630a0a5b4c9f66ed8689c7188
David-HxH <david_ems@live.cl>
2025-11-23 22:01:44 -0300

    Primer commit

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+document.addEventListener(`DOMContentLoaded`, () => {
+    
+    let crearLista = confirm(`Bienvenido a tu lista de tareas ,Deseas crear una lista de tareas?`
+    )
+
+    if (crearLista === true) {
+        let listaDeTareas = [];
+        let tarea = prompt (`Ingresa una tarea para agregarar a la lista de tareas (o presiona cancelar para salir):`)
+
+        while (tarea !== null && tarea.trim() !== ``) {
+            listaDeTareas.push(tarea.trim());
+            const confirmar = confirm(`Deseas agregar otra tarea?`)
+            if (confirmar === true) {
+                tarea = prompt (`Ingresa una tarea para agregarar a la lista de tareas (o presiona cancelar para salir):`)
+
+            }else {
+                console.log("--- Lista de Tareas Pendientes ---");
+                for (let i = 0; i < listaDeTareas.length; i++) {
+                    console.log(`${i + 1}. ${listaDeTareas[i]}`);
+                }
+                break;
+            }
+        }
+
+
+    }else {
+        alert(`Hasta la proxima, vuelve pronto!`)
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
